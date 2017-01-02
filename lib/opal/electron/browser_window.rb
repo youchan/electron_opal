2b9b1198674ba50dd5f1037e089a85c6bd1db7e2
youchan <youchan01@gmail.com>
2017-01-02 13:21:17 +0900

    Fix BrowserWindow initialize parameters not work

--- lib/opal/electron/browser_window.rb
+++ lib/opal/electron/browser_window.rb
@@ -6,7 +6,7 @@ module Electron
 
     def initialize(name, params = {}, debug: false)
       `var { BrowserWindow } = require('electron')`
-      @native = JS.new(`BrowserWindow`, params)
+      @native = JS.new(`BrowserWindow`, `params.$to_n()`)
       @native.JS.loadURL("file://#{`__dirname`}/main_window.html")
       @native.JS[:webContents].JS.openDevTools if debug
     end
