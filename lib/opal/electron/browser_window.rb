dde2663155466577a71879538fa3f75eb433cc2c
Sam Kelly <sam@durosoft.com>
2017-01-02 00:55:35 -0500

    add support for more than one BrowserWindow filename
    * previously all instances of BrowserWindow would ignore the `name`
      parameter and would use "main_window.html".
    * now BrowserWindow's initializer will use the name parameter
      when determining the filename to load

--- lib/opal/electron/browser_window.rb
+++ lib/opal/electron/browser_window.rb
@@ -4,10 +4,10 @@ module Electron
   class BrowserWindow
     include Native
 
-    def initialize(name, params = {}, debug: false)
+    def initialize(name = 'main_window', params = {}, debug: false)
       `var { BrowserWindow } = require('electron')`
       @native = JS.new(`BrowserWindow`, `params.$to_n()`)
-      @native.JS.loadURL("file://#{`__dirname`}/main_window.html")
+      @native.JS.loadURL("file://#{`__dirname`}/#{name}.html")
       @native.JS[:webContents].JS.openDevTools if debug
     end
 
