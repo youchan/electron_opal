38f6a5abba2114b582f45f7d91bc13969be0562f
Sam Kelly <sam@durosoft.com>
2017-01-03 05:08:36 -0500

    add ruby support for BrowserWindow instance methods
    * getters use original method name minus 'get'
    * setters use ruby setter (original_method_name=)
    * booleans use original_method_name?

--- lib/opal/electron/browser_window.rb
+++ lib/opal/electron/browser_window.rb
@@ -1,4 +1,5 @@
 require 'electron'
+require 'electron/string_enhancements'
 
 module Electron
   class BrowserWindow
@@ -9,8 +10,31 @@ module Electron
       @native = JS.new(`BrowserWindow`, `params.$to_n()`)
       @native.JS.loadURL("file://#{`__dirname`}/#{name}.html")
       @native.JS[:webContents].JS.openDevTools if debug
+      methods_ruby = []
+      %x{
+        for(var method in #@native) {
+          #{methods_ruby << `method`}
+        }
+      }
+      methods_ruby.each do |method|
+        next if method[0] == '_'
+        next if method.index('send') == 0
+        BrowserWindow.instance_eval do
+          original = method.to_sym
+          if method.index('is') == 0
+            ruby_name = (underscore(method.sub('is', '')) + '?').to_sym
+          elsif method.index('has') == 0
+            ruby_name = (underscore(method.sub('has', '')) + '?').to_sym
+          elsif method.index('set') == 0
+            ruby_name = (underscore(method.sub('set', '')) + '=').to_sym
+          elsif method.index('get') == 0
+            ruby_name = underscore(method.sub('get', '')).to_sym
+          else
+            ruby_name = underscore(method).to_sym
+          end
+          alias_native ruby_name, original
+        end
+      end
     end
-
-    alias_native :on
   end
 end
